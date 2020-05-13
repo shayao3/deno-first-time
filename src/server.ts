92e60ccb6c82e34ae24a2733a26fccea374f3689
shayao3 <int880double@gmail.com>
2020-05-13 22:25:13 +0900

    Add application

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
-import { app, get } from 'https://denopkg.com/syumai/dinatra/mod.ts';
+import { app, get, post } from "https://denopkg.com/syumai/dinatra/mod.ts";
+import { renderFile } from "https://deno.land/x/dejs/mod.ts";
 
-app(get('/hello', () => 'hello'));
+const templatePath = `${Deno.cwd()}/src/index.ejs`;
+console.log(templatePath);
+
+app(
+  get("/", async () => await renderFile(templatePath, { message: "" })),
+  post(
+    "/posts",
+    async ({ params: { message } }: any) =>
+      await renderFile(templatePath, { message }),
+  ),
+);
